5b6712b4686eabdf521b65107f271877e512baba
Caio Suzano <caiosfs44@gmail.com>
2024-08-21 16:24:54 -0300

    condicionais e operadores lógicos

---
+++ IF/condicionais_com_operadores.js
@@ -0,0 +1,97 @@
+let num = 10;
+
+if (num > 5)
+    {
+        console.log("O número é maior que 5");
+    }
+
+    else
+    {
+        console.log("O número é menor que 5");
+    }
+
+let idade = 16;
+
+if (idade < 18 )
+    {
+        console.log("Você é menor de idade");
+    }
+
+    else
+    {
+        console.log("Você é maior de idade");
+    }
+
+let saldo = 100;
+
+if (saldo >= 0)
+    {
+        console.log("Seu saldo é positivo ou zero");
+    }
+
+    else
+    {
+        console.log("Seu saldo é negativo");
+    }
+
+let temperatura = 25;
+
+if (temperatura <= 0)
+    {
+        console.log("Está muito frio");
+    }
+
+    else
+    {
+        console.log("A temperatura está acima de zero");
+    }
+
+let cor = "vermelho"
+
+if (cor === "vermelho")
+    {
+        console.log("A cor é vermelho");
+    }
+
+    else
+    {
+        console.log("A cor não é vermelho");
+    }
+
+let diaSemana = "sábado";
+
+if (diaSemana !== "domingo")
+    {
+        console.log("Hoje não é domingo");
+    }
+    
+    else
+    {
+        console.log("Hoje é domingo");
+    }
+
+let idadeE = 18;
+
+let temCnh = true;
+
+if (idadeE >= 18 && temCnh)
+    {
+        console.log("Você é maior de idade e possui careteira de motorista");
+    } 
+
+    else
+    {
+        console.log("Você não é maior de idade ou não possui carteira de motorista");
+    }
+
+let diaDaSemana = "sábado";
+
+if (diaDaSemana === "sábado" || diaDaSemana === "domingo")
+    {
+        console.log("É fim de semana");
+    } 
+
+    else
+    {
+        console.log("É dia de semana")
+    }
